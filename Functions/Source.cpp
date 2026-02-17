d5cee465700c8afd4c1a4e260b934a07e9f865c5
EDUARD STEFAN <E.STEFAN-2023@hull.ac.uk>
2026-02-17 21:35:17 +0000

    Added new code for the functions project, and need to improve the readme file for it as well, as i wasnt able to get everything i needed on my laptop :/

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -1,24 +1,54 @@
+//#include <iostream>
+//using namespace std;
+//
+//int mymax(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
+//
+//int main(int, char**) {
+//
+//	int a = 10;
+//	int b = 20;
+//
+//	int max = mymax(a, b);
+//
+//	cout << "a = " << a << ", b = " << b << endl;
+//	cout << "max = " << max << endl;
+//
+//	return 0;
+//}
+
 #include <iostream>
 using namespace std;
 
-int mymax(int a, int b)
-{
-	if (a > b)
-		return a;
-	else
-		return b;
+int mymax(int a, int b) {
+    return (a > b) ? a : b;
 }
 
-int main(int, char**) {
-
-	int a = 10;
-	int b = 20;
-
-	int max = mymax(a, b);
-
-	cout << "a=" << a << ", b=" << b << endl;
-	cout << "max=" << max << endl;
-
-	return 0;
+// New function with 3 different parameter types
+void showValues(int x, double y, char z) {
+    cout << "x (int)    = " << x << endl;
+    cout << "y (double) = " << y << endl;
+    cout << "z (char)   = " << z << endl;
 }
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+     int max = mymax(a, b);
+
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "max = " << max << endl;
+
+    // Call new function
+    showValues(42, 3.14, 'A');
+
+    return 0;
+}
+
 
